7679c54ad92920177bd5c52cd85a7680d818216e
ayush1168 <thakurayush6424@gmail.com>
2022-10-04 12:16:12 +0530

    java is object oriented language

--- Basic.java
+++ Basic.java
@@ -1,6 +1,5 @@
 class Basic{
     //main function of java 
     public static void main(String args[]){
-        System.out.print("Hello to HYTEK with Ayush Thakur JAVA\n");
+        System.out.print("Hello to HYTEK with Ayush Thakur JAVA");
     }
-}
